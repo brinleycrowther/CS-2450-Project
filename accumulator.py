cc1736934be0ed1d3091994d7ca64e9e028b599c
brinleycrowther <168596690+brinleycrowther@users.noreply.github.com>
2025-02-06 21:59:56 -0700

    Added error handling to prevent division by zero

--- accumulator.py
+++ accumulator.py
@@ -29,35 +29,31 @@ class Accumulator:
     #11.. WRITES word from specified location in memory(loc) and outputs it to screen
     def write(self, loc, sign):
         print(self.memory[int(loc)])
-        #print(f"11: writes {self.memory[int(loc)]} from {int(loc)} to screen")
         return self.memory[int(loc)]
 
     # 20.. LOADS word from specific location in memory(loc) into accumulator
     def load(self, loc, sign):
         self.currVal = int(self.memory[int(loc)])
-        #print(f"20: loads {self.memory[int(loc)]} to accumulater: {self.currVal}")
 
     # 21.. STORES word from accumulator into specific location in memory(loc)
     def store(self, loc, sign):
         self.memory[int(loc)] = str(self.currVal)
-        #print(f"21: stores accumulator {self.currVal} into {int(loc)}")
 
     # 30.. ADDS value from specific location in memory(loc) to accumulator currVal
     def add(self, loc, sign):
         self.currVal += int(self.memory[int(loc)])
-        #print(f"30: adds {self.memory[int(loc)]} to accumulator {self.currVal}")
 
     # 31.. SUBTRACTS value from specific location in memory(loc) from accumulator currVal
     def subtract(self, loc, sign):
         self.currVal -= int(self.memory[int(loc)])
-        #print(f"31: subtracts {self.memory[int(loc)]} from accumulator {self.currVal}")
 
     # 32.. DIVIDES value from specific location in memory(loc) from accumulator currVal
     def divide(self, loc, sign):
-        self.currVal /= int(self.memory[int(loc)])
-        #print(f"32: divides {self.memory[int(loc)]} from accumulator {self.currVal}")
+        try:
+            self.currVal /= int(self.memory[int(loc)])
+        except ZeroDivisionError:
+            print("Error: Cannot divide by zero.")
 
     # 33.. MULTIPLIES value from specific location in memory(loc) to accumulator currVal
     def multiply(self, loc, sign):
         self.currVal *= int(self.memory[int(loc)])
-        #print(f"33: multiplies {self.memory[int(loc)]} to accumulator {self.currVal}")
